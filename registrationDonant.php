edfbea8fddb298150772c9fdc95adc674c734666
rsanlazaro <rsanlazaro@hotmail.com>
2023-02-27 07:48:08 -0600

    new

--- registrationDonant.php
+++ registrationDonant.php
@@ -34,7 +34,7 @@ if (isset($_REQUEST['nationality'])) {
         $_SESSION['profile'] = $_REQUEST['profile'];
         $_SESSION['supplier'] = $_REQUEST['supplier'];
         $_SESSION['price'] = $_REQUEST['price'];
-        header("location: /registrationDonant.php?msg=El código de identificación no tiene el formato requerido (texto-números)");
+        // header("location: /registrationDonant.php?msg=El código de identificación no tiene el formato requerido (texto-números)");
     } else {
         $sql = "SELECT * FROM donants WHERE code='${code}'";
         $result = mysqli_query($conn, $sql);
@@ -55,58 +55,58 @@ if (isset($_REQUEST['nationality'])) {
             $_SESSION['profile'] = $_REQUEST['profile'];
             $_SESSION['supplier'] = $_REQUEST['supplier'];
             $_SESSION['price'] = $_REQUEST['price'];
-            header("location: /registrationDonant.php?msg=El código de identificación ya ha sido registrado. Por favor, seleccione otro");
+            // header("location: /registrationDonant.php?msg=El código de identificación ya ha sido registrado. Por favor, seleccione otro");
         } else {
             $profile = stripslashes($_REQUEST['profile']);
             $profile = mysqli_real_escape_string($conn, $profile);
             // Upload image
-            if (!($profile === 'Fenotipe')) {
-                if (isset($_FILES)) {
-                    if (isset($_FILES['image-1']) && isset($_FILES['image-2']) && isset($_FILES['image-3']) && isset($_FILES['image-4'])) {
-                        $file = $_FILES['image-1']['name'];
-                        $file2 = $_FILES['image-2']['name'];
-                        $file3 = $_FILES['image-3']['name'];
-                        $file4 = $_FILES['image-4']['name'];
-                        $path = pathinfo($file);
-                        $path2 = pathinfo($file2);
-                        $path3 = pathinfo($file3);
-                        $path4 = pathinfo($file4);
-                        $ext_img_1 = $path['extension'];
-                        $ext_img_2 = $path2['extension'];
-                        $ext_img_3 = $path3['extension'];
-                        $ext_img_4 = $path4['extension'];
-                        $_FILES['image-1']['name'] = $_REQUEST['code'] . "_1";
-                        $_FILES['image-2']['name'] = $_REQUEST['code'] . "_2";
-                        $_FILES['image-3']['name'] = $_REQUEST['code'] . "_3";
-                        $_FILES['image-4']['name'] = $_REQUEST['code'] . "_4";
-                        $cloudinary->uploadApi()->upload(
-                            $_FILES['image-1']['tmp_name'],
-                            ['public_id' => $_FILES['image-1']['name']]
-                        );
-                        // $cloudinary->uploadApi()->upload(
-                        //     $_FILES['image-2']['tmp_name'],
-                        //     ['public_id' => $_FILES['image-2']['name']]
-                        // );
-                        // $cloudinary->uploadApi()->upload(
-                        //     $_FILES['image-3']['tmp_name'],
-                        //     ['public_id' => $_FILES['image-3']['name']]
-                        // );
-                        // $cloudinary->uploadApi()->upload(
-                        //     $_FILES['image-4']['tmp_name'],
-                        //     ['public_id' => $_FILES['image-4']['name']]
-                        // );
-                        // move_uploaded_file($_FILES['image-1']['tmp_name'], "build/img/admin/donants/" . $_FILES['image-1']['name']);
-                        // move_uploaded_file($_FILES['image-2']['tmp_name'], "build/img/admin/donants/" . $_FILES['image-2']['name']);
-                        // move_uploaded_file($_FILES['image-3']['tmp_name'], "build/img/admin/donants/" . $_FILES['image-3']['name']);
-                        // move_uploaded_file($_FILES['image-4']['tmp_name'], "build/img/admin/donants/" . $_FILES['image-4']['name']);
-                    }
-                }
-            } else {
-                $ext_img_1 = 'png';
-                $ext_img_2 = 'png';
-                $ext_img_3 = 'png';
-                $ext_img_4 = 'png';
-            }
+            // if (!($profile === 'Fenotipe')) {
+            // if (isset($_FILES)) {
+            //     if (isset($_FILES['image-1']) && isset($_FILES['image-2']) && isset($_FILES['image-3']) && isset($_FILES['image-4'])) {
+            //         $file = $_FILES['image-1']['name'];
+            //         $file2 = $_FILES['image-2']['name'];
+            //         $file3 = $_FILES['image-3']['name'];
+            //         $file4 = $_FILES['image-4']['name'];
+            //         $path = pathinfo($file);
+            //         $path2 = pathinfo($file2);
+            //         $path3 = pathinfo($file3);
+            //         $path4 = pathinfo($file4);
+            //         $ext_img_1 = $path['extension'];
+            //         $ext_img_2 = $path2['extension'];
+            //         $ext_img_3 = $path3['extension'];
+            //         $ext_img_4 = $path4['extension'];
+            //         $_FILES['image-1']['name'] = $_REQUEST['code'] . "_1";
+            //         $_FILES['image-2']['name'] = $_REQUEST['code'] . "_2";
+            //         $_FILES['image-3']['name'] = $_REQUEST['code'] . "_3";
+            //         $_FILES['image-4']['name'] = $_REQUEST['code'] . "_4";
+            //         $cloudinary->uploadApi()->upload(
+            //             $_FILES['image-1']['tmp_name'],
+            //             ['public_id' => $_FILES['image-1']['name']]
+            //         );
+            //         $cloudinary->uploadApi()->upload(
+            //             $_FILES['image-2']['tmp_name'],
+            //             ['public_id' => $_FILES['image-2']['name']]
+            //         );
+            //         $cloudinary->uploadApi()->upload(
+            //             $_FILES['image-3']['tmp_name'],
+            //             ['public_id' => $_FILES['image-3']['name']]
+            //         );
+            //         $cloudinary->uploadApi()->upload(
+            //             $_FILES['image-4']['tmp_name'],
+            //             ['public_id' => $_FILES['image-4']['name']]
+            //         );
+            //         move_uploaded_file($_FILES['image-1']['tmp_name'], "build/img/admin/donants/" . $_FILES['image-1']['name']);
+            //         move_uploaded_file($_FILES['image-2']['tmp_name'], "build/img/admin/donants/" . $_FILES['image-2']['name']);
+            //         move_uploaded_file($_FILES['image-3']['tmp_name'], "build/img/admin/donants/" . $_FILES['image-3']['name']);
+            //         move_uploaded_file($_FILES['image-4']['tmp_name'], "build/img/admin/donants/" . $_FILES['image-4']['name']);
+            //     }
+            // }
+            // } else {
+            $ext_img_1 = 'png';
+            $ext_img_2 = 'png';
+            $ext_img_3 = 'png';
+            $ext_img_4 = 'png';
+            // }
             // removes backslashes
             $nationality = stripslashes($_REQUEST['nationality']);
             $nationality = mysqli_real_escape_string($conn, $nationality);
@@ -141,11 +141,11 @@ if (isset($_REQUEST['nationality'])) {
             $query    = "INSERT into `donants` (nationality, date_birth, color_eyes, color_skin, blood_type, height, weight, education, color_hair, type_hair, type_body, ocupation, profile, supplier, price, code, code_img, ext_img_1, ext_img_2, ext_img_3, ext_img_4)
                     VALUES ('$nationality', '" . $date_birth . "', '$color_eyes', '$color_skin', '$blood_type', '$height', '$weight', '$education', '$color_hair', '$type_hair', '$type_body', '$ocupation', '$profile', '$supplier', '$price', '$code', '$code_img', '$ext_img_1', '$ext_img_2', '$ext_img_3', '$ext_img_4')";
             $result   = mysqli_query($conn, $query);
-            if ($result) {
-                header("Location: donants.php?msg=El usuario se ha creado exitosamente");
-            } else {
-                header("Location: donants.php?msg=Hubo un problema registrando al usuario. Por favor, intente nuevamente");
-            }
+            // if ($result) {
+            //     header("Location: donants.php?msg=El usuario se ha creado exitosamente");
+            // } else {
+            //     header("Location: donants.php?msg=Hubo un problema registrando al usuario. Por favor, intente nuevamente");
+            // }
         }
     }
 } else {
@@ -178,7 +178,7 @@ if (isset($_REQUEST['nationality'])) {
             <div class="form-body">
                 <div class="contact-form">
                     <h2 class="contact-form-title">Nuevo donante</h1>
-                        <form class="form" action="" method="post" enctype="multipart/form-data">
+                        <form class="form" action="" method="post" enctype="multipart/form-data" id="file-upload">
                             <div class="col-md-12">
                                 <label for="validationCustom01">Código de identificación</label>
                                 <input type="text" class="form-control" id="validationCustom01" name="code" required />
@@ -406,6 +406,7 @@ if (isset($_REQUEST['nationality'])) {
     </main>
 <?php } ?>
 <script src="build/js/bundle.min.js"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </body>
 
 </html>
