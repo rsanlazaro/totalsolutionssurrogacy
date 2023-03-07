e7e2fae982df4c5125897e5ab72235b6d5730812
rsanlazaro <rsanlazaro@hotmail.com>
2023-03-06 23:15:53 -0600

    selection method fixed

--- registerDonant.php
+++ registerDonant.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $query1 = "SELECT donant_1 FROM users WHERE id='${id}' AND donant_1 IS NULL OR donant_1 = ''";
     $result1 = mysqli_query($conn, $query1);
     $exists1 = $result1->num_rows;
-    if ($exists1 > 1) {
+    if ($exists1 > 0) {
         $query = "UPDATE users SET donant_1='${code}' WHERE id='${id}'";
         $result = mysqli_query($conn, $query);
         header("location: /catalogue.php?msg=Aún puedes seleccionar 2 donantes");
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $query2 = "SELECT donant_2 FROM users WHERE id='${id}' AND donant_2 IS NULL OR donant_2 = ''";
         $result2 = mysqli_query($conn, $query2);
         $exists2 = $result2->num_rows;
-        if ($exists2 > 1) {
+        if ($exists2 > 0) {
             $query = "UPDATE users SET donant_2='${code}' WHERE id='${id}'";
             $result = mysqli_query($conn, $query);
             header("location: /catalogue.php?msg=Aún puedes seleccionar 1 donante");
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $query3 = "SELECT donant_3 FROM users WHERE id='${id}' AND donant_3 IS NULL OR donant_3 = ''";
             $result3 = mysqli_query($conn, $query3);
             $exists3 = $result3->num_rows;
-            if ($exists3 > 1) {
+            if ($exists3 > 0) {
                 $query = "UPDATE users SET donant_3='${code}' WHERE id='${id}'";
                 $result = mysqli_query($conn, $query);
                 header("location: /catalogue.php?msg=Gracias. Ya has seleccionado las 3 donantes");
