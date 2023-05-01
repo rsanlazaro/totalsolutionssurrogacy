197ecc116881a8b108414542298ccbaa1ba67e6d
rsanlazaro <rsanlazaro@hotmail.com>
2023-05-01 09:25:43 -0600

    3 phenotypes added and phenotypes catalogue added to admin and admin-jrs

--- phenotypes.php
+++ phenotypes.php
@@ -111,16 +111,16 @@ while ($row = mysqli_fetch_assoc($result)) {
         filterSelection("fenotipe");
     <?php } ?>
 
-    function filterSelection(c) {
-        var x, i;
-        x = document.getElementsByClassName("column");
-        if (c == "all") c = "";
-        // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-        for (i = 0; i < x.length; i++) {
-            w3RemoveClass(x[i], "show");
-            if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-        }
-    }
+    // function filterSelection(c) {
+    //     var x, i;
+    //     x = document.getElementsByClassName("column");
+    //     if (c == "all") c = "";
+    //     // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    //     for (i = 0; i < x.length; i++) {
+    //         w3RemoveClass(x[i], "show");
+    //         if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    //     }
+    // }
 
     // Show filtered elements
     function w3AddClass(element, name) {
